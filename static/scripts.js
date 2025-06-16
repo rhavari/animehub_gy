014599ca40c00394b454aaf51ca207d39b2b72bb
torontomet <rhavaraj@torontomu.ca>
2025-06-16 17:25:31 -0400

    updated scripts

--- static/scripts.js
+++ static/scripts.js
@@ -3,5 +3,38 @@
 * Copyright 2013-2023 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-one-page-wonder/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+document.addEventListener("DOMContentLoaded", () => {
+    const follower = document.createElement("div");
+    follower.id = "cursor-follower";
+    document.body.appendChild(follower);
+    
+    let mouseX = 0, mouseY = 0;
+    let currentX = 0, currentY = 0;
+    
+    document.addEventListener("mousemove", e => {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+    });
+    
+    function animate() {
+      currentX += (mouseX - currentX) * 0.3;
+      currentY += (mouseY - currentY) * 0.3;
+      
+      follower.style.left = currentX + "px";
+      follower.style.top = currentY + "px";
+      requestAnimationFrame(animate);
+    }
+    
+    animate();
+});
+
+document.addEventListener("click", (e) => {
+  const burst = document.createElement("span");
+  burst.className = "click-burst";
+  burst.style.left = `${e.clientX}px`;
+  burst.style.top = `${e.clientY}px`;
+  document.body.appendChild(burst);
+  setTimeout(() => burst.remove(), 500);
+});
+      
